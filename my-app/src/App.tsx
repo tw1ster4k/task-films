639eca0bf719746e8e275c1be4ead209475ee08b
Amin <anurshinov2006@gmail.com>
2023-03-11 12:17:41 +0500

    Release1.1

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,33 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { cn } from '@bem-react/classname';
+import Card from './Elements/Card/Card';
+import data from "./data.json"
 
 function App() {
-
   const cnApp = cn("App")
   const [genre, setGenre] = useState(0)
+  
 
   const options = [
     {id:0,genre:'Все'},{id:1,genre:'драма'},{id:2,genre:'биография'},{id:3,genre:'история'},{id:4,genre:'фэнтези'},{id:5,genre:'приключения'},{id:6,genre:'боевик'},{id:7,genre:'мультфильм'},{id:8,genre:'комедия'},{id:9,genre:'триллер'},{id:10,genre:'детектив'},{id:11,genre:'фантастика'}
   ];
+
+  const images: string[] = [];
+
+  // @ts-ignore
+  const imagesContext = require.context('./images', false, /\.(png|jpe?g|svg)$/);
+  
+  imagesContext.keys().map((key: string) => {
+    images.push(imagesContext(key));
+  });
+
+   images.sort((a, b) => {
+    const aNumber = parseInt(a.match(/\d+/)?.[0] || '0', 10);
+    const bNumber = parseInt(b.match(/\d+/)?.[0] || '0', 10);
+    return aNumber - bNumber;
+  });
+
 
 
   return (
@@ -26,6 +44,12 @@ function App() {
           </select>
           <input type="text" placeholder='Введите название' className={cnApp("Input")}/>
         </div>
+      </div>
+      <div className={cnApp("Content")}>
+        {data.map((el,index)  =>
+        // @ts-ignore
+          <Card key={index} title={el.name} image={images[index]} genres={options.filter(({id}) => el.genre.includes(id))} date={el.year} />
+        )}
       </div>
     </div>
   );
---
+++ my-app/src/Elements/Card/Card.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@bem-react/classname"
+import "./Card.css"
+
+interface Card {
+    title: string;
+    image: string;
+    genres: object;
+    date: number
+}
+
+const Card = (Card: Card) => {
+    const cnCard = cn("Card")
+    const arr: string[] = [];
+    // @ts-ignore
+       Card.genres.map((el) => arr.push(el.genre))
+       console.log(arr)
+  return (
+    <div className={cnCard()}>
+        <div className={cnCard("Content")}>
+            <img src={Card.image} alt="zzzz" className={cnCard("Image")} />
+            <h4 className={cnCard("Title")}>{Card.title}</h4>
+            <h6 className={cnCard("Genres")}>{arr.join(",")}</h6>
+            <h6 className={cnCard("Date")}>{Card.date}</h6>
+        </div>
+    </div>
+  )
+}
+
+export default Card
